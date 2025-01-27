c19c72ba766873b8f1607ecf08c9b53d6133d6cb
visualabstract <visualabstract.science@gmail.com>
2025-01-27 21:16:49 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function showTab(tabId) {
+            document.querySelectorAll('.tab-content').forEach(tab => {
+                tab.classList.remove('active');
+            });
+
+            document.querySelector(`#${tabId}`).classList.add('active');
+
+            document.querySelectorAll('.tabs button').forEach(btn => {
+                btn.classList.remove('active');
+            });
+
+            event.target.classList.add('active');
+        }
+
+function submitForm(tab) {
+            alert('Form submitted successfully!');
+            window.location.href = '#'; // Replace '#' with success page link.
+        }
